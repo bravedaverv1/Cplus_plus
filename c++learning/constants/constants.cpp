3513627810b535f20a756fa869ec10976bf86005
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-12-30 19:55:26 -0800

    understanding constants in c++. assign a const keyword to a double data type var to prevent its value from being changed

---
+++ c++learning/constants/constants.cpp
@@ -0,0 +1,14 @@
+// CONSTANTS
+// where we dont want the values of a variable to change.
+
+#include <iostream>
+
+int main(){
+  const double pi = 3.14;
+  // pi = 0; // ! this causes an error if not commented out given the const assigned to pi var.
+  // ? const , what is it?
+  // const is a KEYWORD that triggers an error if its variable 
+  // is attempted for change. in other words, const makes it so that 
+  // you cannot change a variables value.
+  return 0;
+}
