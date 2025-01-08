775df554db2eb6f149c238b69ef4b49cf8f6880e
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-08 11:08:52 -0800

    practing compling and executing on .cpp files on mac terminal

---
+++ c++learning/practice/mac_terminal_compiling/compile.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "testing..?" << endl;
+}
+
+// * personally, debatably SLOWER than vs code
+// ! HOWEVER
+// ? how to run a c++ file on mac terminal?
+// 1. in terminal within project folder:
+//    g++ file_name
+// 2. in terminal with project folder:
+//      ./file_name
+// ** TO CONTROL the name 
+// . * in terminal:
+//      g++ file_name.cpp -o descired_name
+// . * to run the new name, in terminal:
+//      ./new_file_name
+
+// ! * ANY CHANGES MADE TO THE ORIGNAL .cpp FILE MUST:
+// recompile the .cpp file to show the changes and run the new recompiled
+// file.
