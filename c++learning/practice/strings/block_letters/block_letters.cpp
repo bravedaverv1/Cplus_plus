61dfb6bb5f0752ae6dabfe4f0c38b5d4f1e18a55
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-08 11:55:57 -0800

    create an output of your initials in block letter string formation

---
+++ c++learning/practice/strings/block_letters/block_letters.cpp
@@ -0,0 +1,20 @@
+// fun fact: my name is David.
+
+// ? PRACTiCE 
+// create an output of your initials in block letter formation. 
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    cout 
+        << "D D D D    R R R R\n"
+        << "D       D  R       R\n"
+        << "D       D  R       R\n"
+        << "D       D  R R R R\n"
+        << "D       D  R   R\n"
+        << "D       D  R     R\n"
+        << "D D D D    R       R\n";
+}
