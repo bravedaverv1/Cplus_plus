eb07a1d553ef348345b8be54e161740ec7e9ab74
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-09 14:27:27 -0800

    apply an or operator to display weekend as an output if it is day 6 or 6

---
+++ c++learning/logical_operators/or/or.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int day = 6;
+
+    // Write the code below:
+    if (day == 6 || day == 7)
+    {
+        cout << "Weekend\n";
+    }
+}
