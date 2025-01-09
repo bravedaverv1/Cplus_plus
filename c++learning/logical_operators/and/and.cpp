c196d659cc0beeaed78ce0c333d964bccfb7f599
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-09 14:01:36 -0800

    apply the and operators to check if hunger and anger  is true to display Hangry

---
+++ c++learning/logical_operators/and/and.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int hunger = true;
+    int anger = true;
+
+    // Write the code below:
+    if (hunger && anger)
+    {
+        cout << "Hangry\n";
+    }
+}
