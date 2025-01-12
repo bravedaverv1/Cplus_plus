cc84cd759e7192b913eee08816cff89a1e28459f
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-11 17:34:36 -0800

    demonstrate how to initialize a vector in c++ by adding values and show the 2 different ways to init

---
+++ c++learning/vector/initializaing_vectors/initializing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+  // * you can initizialize a vector with a value in 2 different ways.
+  // example 1: // * std::vector<double> location = {42.651443, -73.749302};
+  // Here, we are storing a latitude and a longitude.
+  // example 2: // * std::vector<double> location(2);
+  // the above is GREAT for when you know how many but not sure what you will store yet.
+
+int main() {
+
+  std::vector<double> subway_adult = {800, 1200, 1500};
+  
+  // Give subway_child some values:
+  // ? PROMPT :
+  // Initialize the subway_child vector with:
+  // 400, 600, 750
+  // std::vector<double> subway_child;
+  std::vector<double> subway_child = {400, 600, 750};
+  
+}
