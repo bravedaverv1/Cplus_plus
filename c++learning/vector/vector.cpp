83117818a0ac9d62ac1fcf4605a0936cc2d2542e
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-11 17:09:27 -0800

    define another vector name subway_child

--- c++learning/vector/vector.cpp
+++ c++learning/vector/vector.cpp
@@ -2,5 +2,12 @@
 #include <vector>
 
 int main() {
+
   std::vector<double> subway_adult;
+
+  // ? PROMPT
+  // The Tokyo Subway has different payment options for adults and children. 
+  // We have declared a double vector already named subway_adult.
+  std::vector<double> subway_child;
+
 }
