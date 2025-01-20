e6a3f02398224d16afa21a3f0c13da505a558f47
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-19 21:25:41 -0800

    review how to apply a template and test your logic by calling the function twice, one with decimals and the other as integers

---
+++ c++learning/functions/scope_flexibility/review/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "review.hpp"
+
+int main(){
+  std::cout << get_sum(15.00, 13.00) << std::endl;
+  std::cout << get_sum(5, 3) << std::endl;
+}
---
+++ c++learning/functions/scope_flexibility/review/review.hpp
@@ -0,0 +1,4 @@
+template <typename T>
+T get_sum(T num1 , T num2){
+  return num1 += num2;
+}
