6f598c5bc9220b53443bd7cadc572d7d5d0d612b
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-19 22:45:08 -0800

    introduce class attributes and class methods. delcare the methods in the header file and define them in the cpp file

---
+++ c++learning/classes_objects/song.cpp
@@ -0,0 +1,15 @@
+#include "song.hpp"
+// ? why are we including song.hpp ?
+// unlike regular functions, for classes, we must include the header file here.
+
+// add Song method definitions here:
+// * class members defined here.
+// ? how do we make sure it is a known class method and not a regular function being defined?
+// be applying the class name FIRST - see below; Song:: followed by class method name.
+void Song::add_title(std::string new_title){
+  title = new_title;
+}
+// * class member defined here.
+std::string Song::get_title(){
+  return title;
+}
--- c++learning/classes_objects/song.hpp
+++ c++learning/classes_objects/song.hpp
@@ -2,5 +2,24 @@
 class Song{
   // notice: no parathesis after Song? bc it is not a function, it is a class.
 
+  // ? class members , what are they?
+  // Components of a class are called class members.
+  // ? how can you access a classes members?
+  // Just like you can get a string’s length using .length()
+  // you can access class members using the dot operator (object.class_member)
+  // ? what do class members look like?
+  // two types of class members:
+  // * Attributes, also known as member data, consist of information about an instance of the class.
+  // * Methods, also known as member functions, are functions that you can use with an instance of the class. 
+  // We use a . before method names to distinguish them from regular functions.
+
+  // * class attribute below of a string called title.
+    std::string title;
+
+    public:
+    // * class methods delcared below
+    void add_title(std::string new_title);
+    std::string get_title();
+
 };
 // notice the semi colon after the closing curly brace
