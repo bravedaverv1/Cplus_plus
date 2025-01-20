b658e1b806667220660c58cce7786fb1057133b8
DaveTheWay1 <ramirezvazquez826@gmail.com>
2025-01-19 22:21:03 -0800

    introduce classes by create a class called Song in the header file

---
+++ c++learning/classes_objects/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "song.hpp"
+
+// ? classes , what are they?
+// classes can be thought of as a custom data type.
+// A C++ class is a user-defined type.
+// * The class serves as a blueprint for objects, 
+// which are instances of the class (just like age is an instance of int). 
+// An object gets characteristics and behaviors from its class.
+// * We can create an empty C++ class in the header file.
+int main(){
+
+}
---
+++ c++learning/classes_objects/song.hpp
@@ -0,0 +1,6 @@
+// create the song class below
+class Song{
+  // notice: no parathesis after Song? bc it is not a function, it is a class.
+
+};
+// notice the semi colon after the closing curly brace
